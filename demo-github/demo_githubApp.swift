b1413c197fec1f0c60873449c343ef15a1854d69
sumitchandora <chandorasumit1234@gmail.com>
2023-11-04 19:04:20 +0530

    Initial Commit

---
+++ demo-github/demo_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_githubApp.swift
+//  demo-github
+//
+//  Created by Sumit Chandora on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
